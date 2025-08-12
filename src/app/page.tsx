d86d4ac49cb0e610a3f49a6b5a117b7756dd4b92
KR Shanto <krshanto2005@gmail.com>
2025-08-12 21:58:50 +0600

    feat: add inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import React, { Suspense } from "react";
-import { Client } from "./client";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import React from "react";
+import { toast } from "sonner";
 
 export default function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "fuck you" }));
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
+  const onClick = () => {
+    invoke.mutate({ text: "Owalisa" });
+    toast.success("Background job invoked!");
+  };
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={onClick}>Invoke Background Job</Button>
+    </div>
   );
 }
